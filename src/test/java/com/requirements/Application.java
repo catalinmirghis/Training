b22cfacaa70e48ccad6061594a97b53997b795a7
catalinmirghis <catalin.mirghis@evozon.com>
2014-05-13 11:40:27 +0300

    ss

--- src/test/java/com/requirements/Application.java
+++ src/test/java/com/requirements/Application.java
@@ -4,8 +4,8 @@ import net.thucydides.core.annotations.Feature;
 
 public class Application {
     @Feature
-    public class Search {
-        public class SearchByKeyword {}
+    public class Search { //story
+        public class SearchByKeyword {} //Feature
         public class SearchByMultipleKeywords {} //comment3
     }
 }
